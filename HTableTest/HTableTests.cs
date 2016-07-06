5ef4ace8771e6cc2d427a10ccdb25a8a9dbd52e1
bfawber <bfawber@gmail.com>
2016-07-05 21:38:37 -0400

    initial stuff added

---
+++ HTableTest/HTableTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HTable;
+
+namespace HTableTest
+{
+    [TestClass]
+    public class HTableTests
+    {
+        private HTbl<string, string> testTable;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.testTable = new HTbl<string, string>();
+        }
+
+        [TestMethod]
+        public void Pushing_Popping_Adds_and_Removes()
+        {
+            Assert.AreEqual(default(string), testTable.Pop("one"), "Thinks there is a value!");
+            this.testTable.Push("one", "1");
+            this.testTable.Push("one", "one");
+            Assert.AreNotEqual(default(string), testTable.Pop("one"), "Did not get value correctly!");
+            Assert.AreNotEqual(default(string), testTable.Pop("one"), "Did not get value correctly!");
+            Assert.AreEqual(default(string), testTable.Pop("one"), "Thinks there is still a value!");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            
+        }
+    }
+}
---
+++ Htable/HTbl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace HTable
+{
+    public class HTbl<T, K>
+    {
+        private Dictionary<T, Stack<K>> data;
+
+        public HTbl()
+        {
+            this.data = new Dictionary<T, Stack<K>>();
+        }
+
+        public void Push(T key, K value)
+        {
+            if(data.ContainsKey(key))
+            {
+                this.data[key].Push(value);
+            }
+            else
+            {
+                this.data[key] = new Stack<K>();
+                this.data[key].Push(value);
+            }
+        }
+
+        public K Pop(T key)
+        {
+            if( this.data.ContainsKey(key) &&
+                this.data[key] != null && 
+               this.data[key].Count > 0)
+            {
+                return this.data[key].Pop();
+            }
+            else
+            {
+                return default(K);
+            }
+        }
+    }
+}
