f0d76aa3c3c85359f6f4e243f83b7e5838f5e9e6
bfawber <bfawber@gmail.com>
2016-07-09 10:47:38 -0400

    Added methods IsEmpty(), Clear(), and Contains(K value) and a property called Count to keep track of the size of the data structure. Also added tests for each new method.

--- HTableTest/HTableTests.cs
+++ HTableTest/HTableTests.cs
@@ -26,6 +26,30 @@ namespace HTableTest
             Assert.AreEqual(default(string), testTable.Pop("one"), "Thinks there is still a value!");
         }
 
+        [TestMethod]
+        public void Using_Contains_To_Check_Membership()
+        {
+            this.testTable.Push("1", "one");
+            this.testTable.Push("1", "onetwo");
+            this.testTable.Push("2", "one");
+            this.testTable.Push("3", "three");
+            this.testTable.Push("20", "twenty");
+
+            Assert.IsTrue(testTable.Contains("one"));
+            Assert.IsTrue(testTable.Contains("onetwo"));
+            Assert.IsTrue(testTable.Contains("three"));
+            Assert.IsTrue(testTable.Contains("twenty"));
+            Assert.IsFalse(testTable.Contains("20"));
+        }
+
+        [TestMethod]
+        public void Clear_Makes_List_Empty()
+        {
+            testTable.Clear();
+            Assert.AreEqual(default(string), testTable.Pop("one"));
+            Assert.IsTrue(testTable.IsEmpty());
+        }
+
         [TestCleanup]
         public void CleanUp()
         {
--- Htable/HTbl.cs
+++ Htable/HTbl.cs
@@ -5,6 +5,11 @@ namespace HTable
     public class HTbl<T, K>
     {
         private Dictionary<T, Stack<K>> data;
+        public int Count
+        {
+            get;
+            private set;
+        }
 
         public HTbl()
         {
@@ -22,6 +27,7 @@ namespace HTable
                 this.data[key] = new Stack<K>();
                 this.data[key].Push(value);
             }
+            Count++;
         }
 
         public K Pop(T key)
@@ -30,12 +36,37 @@ namespace HTable
                 this.data[key] != null && 
                this.data[key].Count > 0)
             {
+                Count--;
                 return this.data[key].Pop();
             }
             else
             {
                 return default(K);
             }
+        }
+
+        public bool IsEmpty()
+        {
+            if (Count > 0) return false;
+            return true;
+        }
+
+        public bool Contains(K value)
+        {
+            foreach(Stack<K> stack in this.data.Values)
+            {
+                if (stack != null && stack.Count > 0)
+                {
+                    if (stack.Contains(value)) return true;
+                }
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            this.data.Clear();
         }
     }
 }
